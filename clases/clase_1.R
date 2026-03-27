7505b7ee0a97dfc48ce150c980f9a3bc9f3c577e
bastianolea <bastianolea@gmail.com>
2026-03-26 22:50:08 -0300

    apuntes clase 1

--- clases/clase_1.R
+++ clases/clase_1.R
@@ -1,63 +1,115 @@
-# clase 1
-# 2026-03-25
+# Clase 1
+# Introducción al análisis de datos con R para ciencias sociales
+# Bastián Olea Herrera - https://bastianolea.rbind.io
 
-# en el script puedo ejecutar las lineas con control enter
+# diapositivas: https://bastianolea.github.io/curso_intro_R_gratis_v2/
+
+# en el script podemos ejecutar las lineas con control + enter
 1+2
 6-5 # ejemplo de resta
 
-# esto es una resta
-5-9
+# operaciones matemáticas ----
 
-# sigamos
-
+# sumar dos números
 1+1
-"gato"+"perro"
-5+"5"
+
+# sumar muchos números en una sola línea
+4+4+5+5+5+5+5+5+5+5 
+
+# multiplicar números grandes
+89889*65546
+
+# podemos comentar líneas de código para que no se ejecuten
+# 34343-65656+3232*4343
+
+
+## trabajar con texto ----
+
+# en R, el texto se escribe entre comillas
+"gato"
+
+# no se pueden sumar textos como si fueran números
+# "gato" + "perro" # esto genera un error
+
+# tampoco se pueden sumar números con texto!
+# 5 + "5"
 
 
 
 
-# crear objetos
+# asignación de variables ----
+# guardar un valor en una variable usando <-
+
+# guardar el año actual en una variable
 año <- 2026
+# notemos que al crear un objeto éste aparece en el panel de entorno (Environment)
 
+# ver el contenido de la variable
 año
 
+# guardar un texto en una variable
+nombre <- "basti"
 
-edad <- 37
+
+# reasignar valores ----
+
+# podemos cambiar el valor de una variable existente
+año <- 1993
+
+
+## operaciones con variables ----
+
+# crear dos variables con números
+edad <- 32
 año <- 2026
 
+# restar variables (el orden importa)
+edad - año
 año - edad
 
-gatos <- 3
-gatos
 
-
+# ejemplo de una operación un poco más compleja con objetos
 presupuesto = 100000
 pizza = 15000
 presupuesto - (pizza * 4)
 
-# instalacion, 1 vez no más
-# install.packages("readxl")
 
-library(readxl) # para cargar archivos de excel
+# instalación de paquetes ----
+# los paquetes nos permiten extender R con funcionalidades nuevas
+
+# la instalación sólo es necesaria de hacer una vez, ya que es como bajar una app
+
+# instalamos paquete para cargar datos desde excel
+install.packages("readxl")
+
+# luego cargamos el paquete, que es como abrir una app
+library(readxl) 
+
+# ahora usamos una función del paquete para cargar datos
+# las funciones son como pequeños programas que realizan acciones
+# en este caso, la función "read_excel" carga un archivo si le das la ruta al mismo
 
 datos <- read_excel("estimaciones_pobreza.xlsx")
+# creamos un objeto "datos" que contiene los datos del archivo
 
 datos
 
-# datos <- read_excel("~/Documents/Basty/Clases/perritos.xlsx")
 
-
-
+# ahora instalamos {dplyr}, paquete para trabajar con datos
 install.packages("dplyr")
 
+# cargamos el paquete
 library(dplyr)
 
+# nos entrega funciones para explorar datos
 glimpse(datos)
 
-select(datos, comuna, region)
+# seleccionar columnas
+datos |> select(comuna, region)
+# esto significa: "al objeto 'datos' luego le selecciono las columnas comuna y region"
 
-
+# seleccionar y ordenar columnas
 datos |> 
   select(region, comuna, personas) |> 
   arrange(personas)
+# "al objeto 'datos' luego le selecciono 3 columnas y ordeno los datos según la variable personas"
